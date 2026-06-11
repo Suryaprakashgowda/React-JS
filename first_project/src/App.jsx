19fd7d5d83a02ffa7683ca4a6e7a9163f5d4beb3
Suryaprakashgowda H S <srinivasagowdabv66@gmail.com>
2026-06-11 18:29:20 +0530

    first commit

---
+++ first_project/src/App.jsx
@@ -0,0 +1,24 @@
+import Navbar from "./components/Navbar"
+import React from 'react'
+const App = () => {
+  console.log("Welcome");
+  
+  return (
+    <>
+
+      {/* <h1>Surya</h1>
+      <h2>Welcome</h2> */}
+      <Navbar/>   
+       <Navbar/>
+        <Navbar/>
+         <Navbar/>
+          <Navbar/>
+           <Navbar/>
+            <Navbar/>
+
+    
+    </>
+  )
+}
+
+export default App
---
+++ first_project/src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Navbar=()=>{
+  return (
+    <>
+    <div className="nav">
+      <div className="left">
+      <h2>Logo</h2>
+      </div>
+      <div className="right">
+        <h2>Sigup</h2>
+        <h2>login</h2>
+      </div>
+    </div>
+    </>
+  )
+}
+export default Navbar
