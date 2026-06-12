3006e9a9ccf3ad0479f66b4cba0d06a6518a45c1
Suryaprakashgowda H S <srinivasagowdabv66@gmail.com>
2026-06-12 08:15:50 +0530

    second_project

---
+++ second_project/src/App.jsx
@@ -0,0 +1,11 @@
+import Homepage from './pages/Homepage'
+import React from 'react'
+const App=()=>{
+  return (
+    <>
+    {/* <h1>App Component</h1> */}
+    <Homepage/>
+    </>
+  )
+}
+export default App
---
+++ second_project/src/pages/Homepage.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const Homepage=()=>{
+    return (
+        <>
+            <div className='hero-section'>
+            <h1>Welcome to our site</h1>
+            <button>Shop Now</button>
+            </div>
+            <center><h1>Products</h1></center>
+            <div className="products">
+                <img src="https://m.media-amazon.com/images/I/71MvQEZCXPL._SL1317_.jpg" alt="" />
+                <img src="https://m.media-amazon.com/images/I/71fLfA8RxvL._SL1500_.jpg" alt="" />
+                <img src="https://m.media-amazon.com/images/I/81lcT7VI-sL._SL1500_.jpg" alt="" />
+            </div>
+            <div className="products">
+                <img src="https://m.media-amazon.com/images/I/81vF93kth3L._SL1500_.jpg" alt="" />
+                <img src="https://m.media-amazon.com/images/I/71-nLmQOYYL._SL1500_.jpg" alt="" />
+                <img src="https://universalbook.in/wp-content/uploads/2025/05/KCET-Book-Cover-Page-Phy-scaled.jpg" alt="" />
+            </div>
+            <div className="products">
+                <img src="https://th.bing.com/th/id/R.7d2ed41fe579e5694cdcf78de34583dc?rik=F1ImaRWQg8EKcw&riu=http%3a%2f%2frukmini1.flixcart.com%2fimage%2f300%2f300%2fxif0q%2fbook%2fp%2fi%2f8%2fnest-kcet-mathematics-original-imagmpyzqkkhf3qg.jpeg&ehk=FIKbUs%2bTGkwbZb10OexQ9cHJ7Wr1QSnotfiXhxQJU%2bc%3d&risl=&pid=ImgRaw&r=0" alt="" />
+                <img src="https://m.media-amazon.com/images/I/513YDHsOydL.jpg" alt="" />
+                <img src="https://m.media-amazon.com/images/I/61kFytafhKL._SL1310_.jpg" alt="" />
+            </div>
+
+        </>
+    )
+}
+export default Homepage
